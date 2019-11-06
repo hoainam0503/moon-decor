f796f94ee43ed82d1c4257acec3eede244c07fa9
hoainam0503 <14520549@gm.uit.edu.vn>
2019-11-06 08:18:32 +0700

    init

--- src/component/Test.js
+++ src/component/Test.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-
+import * as img11 from './item/image/img11.png';
+import * as img12 from './item/image/img12.jpg';
+import * as img13 from './item/image/img13.jpg';
 class Test extends Component {
   render() {
     return (
@@ -7,7 +9,7 @@ class Test extends Component {
         <div className="main">
           <div className="page_container">
             <div id="immersive_slider">
-              <div className="slide" data-blurred="https://picture.vn/wp-content/uploads/2019/08/Ch%E1%BB%A5p-%E1%BA%A3nh-phong-c%C3%A1ch-h%C3%A0n-qu%E1%BB%91c-4.jpg">
+              <div className="slide" data-blurred={img11}>
                 <div className="content">
                   <h2><a href="http://www.bucketlistly.com" target="_blank">BucketListly</a></h2>
                   <p>It’s never been easier to watch YouTube on the big screen
@@ -15,28 +17,28 @@ class Test extends Component {
                 </div>
                 <div className="image">
                   <a href="http://www.bucketlistly.com" target="_blank">
-                    <img src="https://picture.vn/wp-content/uploads/2019/08/Ch%E1%BB%A5p-%E1%BA%A3nh-phong-c%C3%A1ch-h%C3%A0n-qu%E1%BB%91c-4.jpg" alt="Slider 1" />
+                    <img src={img11} alt="Slider 1" />
                   </a>
                 </div>
               </div>
-              <div className="slide" data-blurred="https://sohanews.sohacdn.com/thumb_w/660/2019/photo1560163835071-1560163835853-crop-15601638570911960037640.jpg">
+              <div className="slide" data-blurred={img12}>
                 <div className="content">
                   <h2><a href="http://www.bucketlistly.com/apps" target="_blank">BucketListly Apps</a></h2>
                   <p>It’s never been easier to watch YouTube on the big screen
               Send your favorite YouTube videos from your Android phone or tablet to TV with the touch of a button. It’s easy. No wires, no setup, no nothing. Find out more here.</p>
                 </div>
                 <div className="image">
-                  <a href="http://www.bucketlistly.com/apps" target="_blank"> <img src="https://sohanews.sohacdn.com/thumb_w/660/2019/photo1560163835071-1560163835853-crop-15601638570911960037640.jpg" alt="Slider 1" /></a>
+                  <a href="http://www.bucketlistly.com/apps" target="_blank"> <img src={img12} alt="Slider 1" /></a>
                 </div>
               </div>
-              <div className="slide" data-blurred="https://atpweb.vn/blog/wp-content/uploads/2019/08/10-trang-web-c%C3%B3-ngu%E1%BB%93n-h%C3%ACnh-%E1%BA%A3nh-mi%E1%BB%85n-ph%C3%AD-ch%E1%BA%A5t-l%C6%B0%E1%BB%A3ng-3.jpeg">
+              <div className="slide" data-blurred={img13}>
                 <div className="content">
                   <h2><a href="http://www.thepetedesign.com" target="_blank">The Pete Design</a></h2>
                   <p>It’s never been easier to watch YouTube on the big screen
               Send your favorite YouTube videos from your Android phone or tablet to TV with the touch of a button. It’s easy. No wires, no setup, no nothing. Find out more here.</p>
                 </div>
                 <div className="image">
-                  <a href="http://www.thepetedesign.com" target="_blank"><img src="https://atpweb.vn/blog/wp-content/uploads/2019/08/10-trang-web-c%C3%B3-ngu%E1%BB%93n-h%C3%ACnh-%E1%BA%A3nh-mi%E1%BB%85n-ph%C3%AD-ch%E1%BA%A5t-l%C6%B0%E1%BB%A3ng-3.jpeg" alt="Slider 1" /></a>
+                  <a href="http://www.thepetedesign.com" target="_blank"><img src={img13} alt="Slider 1" /></a>
                 </div>
               </div>
               <a href="#" className="is-prev">«</a>
